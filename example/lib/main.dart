bc7607797b7eebe72f05821f5b1da537c9bd90f9
Rhydian Thomas <rhydiant@gmail.com>
2021-10-01 16:38:08 +1000

    Migrate to null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
   bool isJailBroken = false;
   bool canMockLocation = false;
   bool isRealDevice = true;
@@ -33,13 +32,13 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     // Platform messages may fail, so we use a try/catch PlatformException.ßß
-    try{
+    try {
       isJailBroken = await TrustFall.isJailBroken;
       canMockLocation = await TrustFall.canMockLocation;
       isRealDevice = await TrustFall.isRealDevice;
       isOnExternalStorage = await TrustFall.isOnExternalStorage;
       isTrustFall = await TrustFall.isTrustFall;
-    }catch(error){
+    } catch (error) {
       print(error);
     }
 
@@ -61,77 +60,92 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           child: Card(
-             child: Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 mainAxisSize: MainAxisSize.min,
-                 children:<Widget>[
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: <Widget>[
-                       Text('isJailBroken():'),
-                       SizedBox(
-                         width: 8,
-                       ),
-                       Text('${isJailBroken ? "Yes" : "No"}', style: TextStyle(fontWeight: FontWeight.w600),),
-                     ],
-                   ),
-                   SizedBox(
-                     height: 8,
-                   ),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: <Widget>[
-                       Text('canMockLocation():'),
-                       SizedBox(
-                         width: 8,
-                       ),
-                       Text('${canMockLocation ? "Yes" : "No"}', style: TextStyle(fontWeight: FontWeight.w600),),
-                     ],
-                   ),
-                   SizedBox(
-                     height: 8,
-                   ),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: <Widget>[
-                       Text('isRealDevice():'),
-                       SizedBox(
-                         width: 8,
-                       ),
-                       Text('${isRealDevice ? "Yes" : "No"}', style: TextStyle(fontWeight: FontWeight.w600),),
-                     ],
-                   ),
-                   SizedBox(
-                     height: 8,
-                   ),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: <Widget>[
-                       Text('isOnExternalStorage():'),
-                       SizedBox(
-                         width: 8,
-                       ),
-                       Text('${isOnExternalStorage ? "Yes" : "No"}', style: TextStyle(fontWeight: FontWeight.w600),),
-                     ],
-                   ),SizedBox(
-                     height: 8,
-                   ),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: <Widget>[
-                       Text('isTrustFall():'),
-                       SizedBox(
-                         width: 8,
-                       ),
-                       Text('${isTrustFall ? "Yes" : "False"}', style: TextStyle(fontWeight: FontWeight.w600),),
-                     ],
-                   ),
-                 ] ,
-               ),
-
-             ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('isJailBroken():'),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        '${isJailBroken ? "Yes" : "No"}',
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('canMockLocation():'),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        '${canMockLocation ? "Yes" : "No"}',
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('isRealDevice():'),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        '${isRealDevice ? "Yes" : "No"}',
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('isOnExternalStorage():'),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        '${isOnExternalStorage ? "Yes" : "No"}',
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('isTrustFall():'),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        '${isTrustFall ? "Yes" : "False"}',
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/generated/i18n.dart
+++ lib/generated/i18n.dart
@@ -12,13 +12,12 @@ class S implements WidgetsLocalizations {
   const S();
 
   static const GeneratedLocalizationsDelegate delegate =
-    GeneratedLocalizationsDelegate();
+      GeneratedLocalizationsDelegate();
 
-  static S of(BuildContext context) => Localizations.of<S>(context, S);
+  static S? of(BuildContext context) => Localizations.of<S>(context, S);
 
   @override
   TextDirection get textDirection => TextDirection.ltr;
-
 }
 
 class $en extends S {
@@ -34,8 +33,9 @@ class GeneratedLocalizationsDelegate extends LocalizationsDelegate<S> {
     ];
   }
 
-  LocaleListResolutionCallback listResolution({Locale fallback, bool withCountry = true}) {
-    return (List<Locale> locales, Iterable<Locale> supported) {
+  LocaleListResolutionCallback listResolution(
+      {Locale? fallback, bool withCountry = true}) {
+    return (List<Locale>? locales, Iterable<Locale> supported) {
       if (locales == null || locales.isEmpty) {
         return fallback ?? supported.first;
       } else {
@@ -44,8 +44,9 @@ class GeneratedLocalizationsDelegate extends LocalizationsDelegate<S> {
     };
   }
 
-  LocaleResolutionCallback resolution({Locale fallback, bool withCountry = true}) {
-    return (Locale locale, Iterable<Locale> supported) {
+  LocaleResolutionCallback resolution(
+      {Locale? fallback, bool withCountry = true}) {
+    return (Locale? locale, Iterable<Locale> supported) {
       return _resolve(locale, fallback, supported, withCountry);
     };
   }
@@ -53,12 +54,12 @@ class GeneratedLocalizationsDelegate extends LocalizationsDelegate<S> {
   @override
   Future<S> load(Locale locale) {
     final String lang = getLang(locale);
-    if (lang != null) {
+    if (lang.isNotEmpty) {
       switch (lang) {
         case "en":
           return SynchronousFuture<S>(const $en());
         default:
-          // NO-OP.
+        // NO-OP.
       }
     }
     return SynchronousFuture<S>(const S());
@@ -73,7 +74,8 @@ class GeneratedLocalizationsDelegate extends LocalizationsDelegate<S> {
   ///
   /// Internal method to resolve a locale from a list of locales.
   ///
-  Locale _resolve(Locale locale, Locale fallback, Iterable<Locale> supported, bool withCountry) {
+  Locale _resolve(Locale? locale, Locale? fallback, Iterable<Locale> supported,
+      bool withCountry) {
     if (locale == null || !_isSupported(locale, withCountry)) {
       return fallback ?? supported.first;
     }
@@ -93,30 +95,28 @@ class GeneratedLocalizationsDelegate extends LocalizationsDelegate<S> {
   /// Returns true if the specified locale is supported, false otherwise.
   ///
   bool _isSupported(Locale locale, bool withCountry) {
-    if (locale != null) {
-      for (Locale supportedLocale in supportedLocales) {
-        // Language must always match both locales.
-        if (supportedLocale.languageCode != locale.languageCode) {
-          continue;
-        }
+    for (Locale supportedLocale in supportedLocales) {
+      // Language must always match both locales.
+      if (supportedLocale.languageCode != locale.languageCode) {
+        continue;
+      }
 
-        // If country code matches, return this locale.
-        if (supportedLocale.countryCode == locale.countryCode) {
-          return true;
-        }
+      // If country code matches, return this locale.
+      if (supportedLocale.countryCode == locale.countryCode) {
+        return true;
+      }
 
-        // If no country requirement is requested, check if this locale has no country.
-        if (true != withCountry && (supportedLocale.countryCode == null || supportedLocale.countryCode.isEmpty)) {
-          return true;
-        }
+      // If no country requirement is requested, check if this locale has no country.
+      if (true != withCountry &&
+          (supportedLocale.countryCode == null ||
+              supportedLocale.countryCode!.isEmpty)) {
+        return true;
       }
     }
     return false;
   }
 }
 
-String getLang(Locale l) => l == null
-  ? null
-  : l.countryCode != null && l.countryCode.isEmpty
+String getLang(Locale l) => l.countryCode != null && l.countryCode!.isEmpty
     ? l.languageCode
     : l.toString();
